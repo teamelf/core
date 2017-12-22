c021c738f7e1e221833d88228959401791d65efc
GuessEver <guessever@gmail.com>
2017-12-22 22:48:40 +0800

    embed images

--- src/Mailer/Mailer.php
+++ src/Mailer/Mailer.php
@@ -125,6 +125,16 @@ class Mailer
      */
     public function view($template, $data = [])
     {
+        $data['embedLogo'] = $this->message->embed(new \Swift_Image(
+            file_get_contents(__DIR__ . '/../../assets/static/logo.png'),
+            'logo.png',
+            'image/png'
+        ));
+        $data['embedBg'] = $this->message->embed(new \Swift_Image(
+            file_get_contents(__DIR__ . '/../../assets/static/bg.png'),
+            'bg.png',
+            'image/png'
+        ));
         $html = ViewService::getEngine()
             ->render($template, $data);
         $this->message->setBody($html, 'text/html');
